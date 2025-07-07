75577e16212feaed1656000344d67a51d407210d
Abhigyan0202 <bhatnagarabhigyan2004@gmail.com>
2025-07-07 23:27:33 +0530

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [n,setN] = useState(0);
+  function startGame(){
+      setN(document.getElementById('n').value);
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>N-dim-tic-tac-toe</h1>
+        <label htmlFor="">Select value of N: </label>
+        <input type="number" id='n' />
+        <button onClick={startGame}>Start game</button>
+        {/* On clicking start game, I can render a html grid right ????? */}
+      <br />
+      {n}
     </>
   )
 }
